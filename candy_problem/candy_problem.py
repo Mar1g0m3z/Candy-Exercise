318468afc89abd107c15c724dd8f6bbf467f44be
marisela <mariselaalejandragomez@gmail.com>
2025-03-18 11:44:01 -0700

    finished the first function of get_friends_favorite candy_count

--- candy_problem/candy_problem.py
+++ candy_problem/candy_problem.py
@@ -1,13 +1,44 @@
 def get_friends_favorite_candy_count(favorites):
-    pass
+    # return a dictionary of candy names and the times it appears in friend favorite list
+    # key should be name of candy, value should be number of times the candy appears in friend favorites.
+    # create a new return dictonary
+    # loop through favorites  to get the friends, then candy from the list
+    # add counter of the amount of times a specific candy gets made
+    fav_candy_count = {}
+    # count = 0
+    # print(favorites)
+    for friends in favorites:
+        # print(friends)
+        for candy in friends[1]:
+            # print(candy)
+            if candy not in fav_candy_count:
+                fav_candy_count[candy] = 1
+            else:
+                fav_candy_count[candy] += 1
+
+        # for candies in friends:
+        # print(candies[1])
+    # print(fav_candy_count)
+    return fav_candy_count
+    # pass
+
 
 def create_new_candy_data_structure(data):
-    pass 
+    pass
+
 
 def get_friends_who_like_specific_candy(data, candy_name):
     pass
 
+
 def create_candy_set(data):
-    pass 
+    pass
 
 
+favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy"]],
+    ["Bob", ["milky way", "licorice", "lollipop"]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+]
+print(get_friends_favorite_candy_count(favorites))
--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -1,41 +1,44 @@
 import pytest
-from candy_problem.candy_problem import * 
+from candy_problem.candy_problem import *
 
 
 def test_create_candy_data_structure_type():
     # Arrange
     friend_favorites = [
-        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
-        [ "Bob", ["milky way", "licorice", "lollipop"]],
-        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
-        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+        ["Sally", ["lollipop", "bubble gum", "laffy taffy"]],
+        ["Bob", ["milky way", "licorice", "lollipop"]],
+        ["Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        ["Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
     ]
 
     # Act
     new_candy_data = create_new_candy_data_structure(friend_favorites)
-    
+
     # Assert
     assert type(new_candy_data) == dict
 
 # Add your own assertions to the test below
+
+
 def test_create_candy_data_structure_values():
 
     # Arrange
     friend_favorites = [
-        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
-        [ "Bob", ["milky way", "licorice", "lollipop"]],
-        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
-        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+        ["Sally", ["lollipop", "bubble gum", "laffy taffy"]],
+        ["Bob", ["milky way", "licorice", "lollipop"]],
+        ["Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        ["Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
     ]
 
     # Act
     new_candy_data = create_new_candy_data_structure(friend_favorites)
-    
+
     # Assert
     assert len(new_candy_data) == 8
     '''
     Add your assertions here!
     '''
+
 
 '''
 5. 
